6334600bde79e39f4c365a834ae44c8816be8a02
Dilmurod <mr.j0k3r.d1ma@gmail.com>
2023-09-23 13:58:30 +0500

    ASD

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require("./app");
 
-const port = process.env.PORT || 2691;
+const port =  2691;
 const server = http.createServer(app);
 
 server.listen(port,()=>{
