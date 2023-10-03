3d222f946c54d364cc4486874e172f56f583ee22
Dilmurod <mr.j0k3r.d1ma@gmail.com>
2023-10-03 15:10:27 +0500

    sdf

--- knexfile.js
+++ knexfile.js
@@ -4,8 +4,8 @@ module.exports = {
 		connection: {
 			host: process.env.DB_HOST || 'localhost',
 			database: process.env.DB_DBNAME || 'admin_it_academy',
-			user: process.env.DB_USERNAME || 'root',//admin_it_academy
-			password: process.env.DB_PASSWORD || '',//#ITacademy123
+			user: process.env.DB_USERNAME || 'admin_it_academy',//admin_it_academy
+			password: process.env.DB_PASSWORD || '#ITacademy123',//#ITacademy123
 		},
 		pool: {
 			min: 0,
