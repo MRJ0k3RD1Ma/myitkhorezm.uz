045e8e90e222a919c5ffd6460c4298466f641832
Dilmurod <mr.j0k3r.d1ma@gmail.com>
2023-10-03 15:56:39 +0500

    sdf

--- router/course.js
+++ router/course.js
@@ -5,6 +5,8 @@ const {protect, authorize} = require("../middleware/auth");
 const router = express.Router();
 
 router.get('/',protect, CourseController.all)
+router.post('/create',protect, CourseController.create)
+router.post('/update/:id',protect, CourseController.update)
 
 
 
